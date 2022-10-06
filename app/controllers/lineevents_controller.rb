48dac89aa55be97c9f98c9b792ecf36c839b7619
Koichi-U <example@example.com>
2022-10-06 14:06:26 +0000

    Deploy to heroku

--- app/controllers/lineevents_controller.rb
+++ app/controllers/lineevents_controller.rb
@@ -15,7 +15,7 @@ class LineeventsController < ApplicationController
   
   def callback
     p 'client: '
-    p　ENV["LINE_CHANNEL_ID"]
+    p　client
     body = request.body.read
     signature = request.env['HTTP_X_LINE_SIGNATURE']
     unless client.validate_signature(body, signature)
