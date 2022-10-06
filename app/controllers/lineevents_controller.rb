c0ded1ebd6a1fc4ee5c5ebe58bde0b6d60acdb7b
Koichi-U <example@example.com>
2022-10-06 15:03:00 +0000

    Deploy to heroku

--- app/controllers/lineevents_controller.rb
+++ app/controllers/lineevents_controller.rb
@@ -15,10 +15,10 @@ class LineeventsController < ApplicationController
   
   def callback
     
-    client = Line::Bot::Client.new { |config|
-      config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
-      config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
-    }
+    # client = Line::Bot::Client.new { |config|
+    #   config.channel_secret = ENV["LINE_CHANNEL_SECRET"]
+    #   config.channel_token = ENV["LINE_CHANNEL_TOKEN"]
+    # }
     
     
     
