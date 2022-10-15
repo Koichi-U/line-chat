2baa5868700269dbfbf114a530f9fc23cca6742a
Koichi-U <example@example.com>
2022-10-15 17:49:06 +0000

    Set a ajax in list page

--- app/controllers/messages/persons_controller.rb
+++ app/controllers/messages/persons_controller.rb
@@ -2,9 +2,6 @@ class Messages::PersonsController < ApplicationController
   def index
     @messages = Chat.where(lineuser_id: params[:lineuser_id])
     
-    respond_to do |format|
-      format.html { redirect_to :root }
-      format.json { render json: @messages }
-    end
+    render partial: "messages", locals: { messages: @messages, lineuser_id: params[:lineuser_id] }
   end
 end
